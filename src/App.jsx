a1677c85a0d3d20f78a9e400f32b5ea83f635a15
Pranjali Mishra <pranjali2413067@akgec.ac.in>
2026-03-29 21:35:49 +0530

    NoProjectPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import ProjectSideBar from "./Components/ProjectSideBar";
 import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar/>
-      <NewProject/>
+  <NoProjectSelected/>
     </main>
   );
 }
---
+++ src/Components/NoProjectSelected.jsx
@@ -0,0 +1,14 @@
+import noProject from '../assets/no-projects.png';
+import Button from './Button.jsx';
+export default function NoProjectSelected(){
+    return <div className='mt-24 text-center w-2/3'>
+        <img src={noProject} alt='empty task list'
+        className='w-16 h-16 mx-auto object-contain'/>
+        <h2 className='font-bold text-stone-500 text-xl my-4' >No Project Selected</h2>
+        <p className='text-stone-400 mb-4'>Select a project or start with new one </p>
+
+        <p>
+           <Button>Create Project</Button>
+        </p>
+    </div>
+}
